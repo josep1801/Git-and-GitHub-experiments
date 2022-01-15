a2842fdff9a85ef45a21ec2f06623ae8cf541df9
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2022-01-15 15:37:09 +0800

    to see updated main branch

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 CLIENT_ACCESS_TOKEN = os.getenv("CLIENT_ACCESS_TOKEN")
 
+# Hey it works!
 print(CLIENT_ACCESS_TOKEN)
+
+""""
+git pull origin main: to pull updated main branch
+"""
