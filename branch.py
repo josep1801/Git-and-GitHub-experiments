e0760a8ab102e3beb62b2b1a10974a47597afebe
Josephine Tan Xin Yi <josephine_tan@mymail.sutd.edu.sg>
2022-01-15 15:01:47 +0800

    some comments are made on unmerged branches

---
+++ branch.py
@@ -0,0 +1,17 @@
+""" 
+Variation 1 branch is like a feature branch which also contains
+the same files as the main branch. 
+
+If satisfied, you can merge it back into the main branch
+
+Recall: 
+-- git branch -a : to see all branches and which branch you are on --
+1. git branch <branch-name> : makes the branch
+2. git checkout <branch-name> : move to that branch 
+3. git add . 
+4. git commit -m "..."
+5. git push (as a separate branch)
+-- git branch -D <branch name>: to delete unmerged branch --
+"""
+
+print("hello world!")
